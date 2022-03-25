42fe58de677cbdcaffec546c1a31a61d343de463
karolsimon <karolsimon95@gmail.com>
2022-03-25 21:12:51 +0100

    commit

---
+++ java.js
@@ -0,0 +1,16 @@
+function getDogsAge(age) {
+  console.log(
+    "karol i dont care about the logic, i am just printing whater you type here"
+  );
+  const results = age * 10.5;
+  return results;
+}
+
+getDogsAge(1);
+getDogsAge(2);
+getDogsAge(3);
+getDogsAge(4);
+getDogsAge(15);
+getDogsAge(1);
+getDogsAge(1);
+getDogsAge(1);
