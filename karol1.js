2f71051bf878fca221f2c8fe520e732182a36cab
karolsimon <karolsimon95@gmail.com>
2022-03-26 17:55:56 +0100

    17:55

---
+++ karol1.js
@@ -0,0 +1,4 @@
+let userName = "Karol";
+userName ? console.log(`Hello ${userName}!`) : console.log("Hello!");
+const userQuestion = "How old are you?";
+console.log(`${userQuestion} ${userName}.`);
