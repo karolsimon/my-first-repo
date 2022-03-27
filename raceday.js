20754f8fe6790a49a62b43cc3fb169f1e14f8df7
karolsimon <karolsimon95@gmail.com>
2022-03-27 15:52:48 +0200

    racedaay

---
+++ raceday.js
@@ -0,0 +1,14 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+const registeredEarly = true;
+const registeredLate = true;
+const age = 99;
+if (registeredEarly === false && age > 18) {
+  raceNumber += 1000;
+  console.log(raceNumber);
+} else if (registeredEarly === true && age > 18) {
+  console.log(
+    `${raceNumber} You are registered late and will race at 11:00 am.`
+  );
+} else if (age <= 18) {
+  console.log(`Your number is ${raceNumber} You will race at 12:30 pm.`);
+}
