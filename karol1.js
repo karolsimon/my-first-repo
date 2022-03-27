aca17c37e80b506c4dd43cc4a45c28a3cd9e999c
karolsimon <karolsimon95@gmail.com>
2022-03-27 15:45:26 +0200

    karol

--- karol1.js
+++ karol1.js
@@ -17,3 +17,17 @@ if (registeredEarly === false && age > 18) {
 } else if (age <= 18) {
   console.log(`Your number is ${raceNumber} You will race at 12:30 pm.`);
 }
+function getDogsAge(age) {
+  console.log(
+    "karol i dont care about the logic, i am just printing whater you type here"
+  );
+}
+
+getDogsAge(1);
+getDogsAge(2);
+getDogsAge(3);
+getDogsAge(4);
+getDogsAge(15);
+getDogsAge(1);
+getDogsAge(1);
+getDogsAge(1);
