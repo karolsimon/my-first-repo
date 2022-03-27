a00cf17b2bf64b6fb7a8d6f9a9e2259fc46c438b
karolsimon <karolsimon95@gmail.com>
2022-03-27 20:46:31 +0200

    michu helped me with this one

---
+++ michu.ts
@@ -0,0 +1,12 @@
+const getUserChoice = (userInput: string) => {
+  userInput = userInput.toLowerCase();
+  if (
+    userInput === "rock" ||
+    userInput === "paper" ||
+    userInput === "scissors"
+  ) {
+    return userInput;
+  } else {
+    console.log("Error!");
+  }
+};
