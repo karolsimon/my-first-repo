6c214651d5c59dc9f48e9c271d95af05b67c5d19
karolsimon <karolsimon95@gmail.com>
2022-03-31 19:16:49 +0200

    karol

--- karolek.ts
+++ karolek.ts
@@ -19,3 +19,60 @@ const onlyNumbers = things.filter((thing) => {
 });
 
 console.log(onlyNumbers);
+
+const fruits = ["mango", "papaya", "pineapple", "apple"];
+
+// Iterate over fruits below
+
+fruits.forEach((fruits) => console.log("I want to eat a " + fruits));
+
+// new exercise
+
+const animals = [
+  "Hen",
+  "elephant",
+  "llama",
+  "leopard",
+  "ostrich",
+  "Whale",
+  "octopus",
+  "rabbit",
+  "lion",
+  "dog",
+];
+
+// Create the secretMessage array below - how to get the first letter of a string in an array and create the new one with .map()
+const secretMessage = animals.map((animals) => animals[0]);
+
+console.log(secretMessage.join(""));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below - how to devide numbers within an array and create the new one with .map()
+const smallNumbers = bigNumbers.map((num) => num / 100);
+console.log(smallNumbers);
+
+/* new exercise
+new exercise */
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below with a val<250 and create a new array
+const smallaNumbers = randomNumbers.filter((randomNumbers) => {
+  return randomNumbers < 250;
+});
+
+const favoriteWords = [
+  "nostalgia",
+  "hyperbole",
+  "fervent",
+  "esoteric",
+  "serene",
+];
+
+// Call .filter() on favoriteWords below that are longer than 7 characters and create a new array
+
+const longFavoriteWords = favoriteWords.filter((favoriteWords) => {
+  return favoriteWords.length > 7;
+});
+console.log(smallaNumbers);
